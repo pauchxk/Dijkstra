f77bf6c5e091c075f92a569179cae00cc7675d4d
James McCann <58793330+pauchxk@users.noreply.github.com>
2023-10-18 12:43:23 +0100

    Update djikstra.py

--- djikstra.py
+++ djikstra.py
@@ -1,17 +1,58 @@
+import adjacency_matrix
+
 class Djikstra:
-    #12/10/23: need to create the graph structure. need a list of nodes, and a dictionary of edges and their weights.
+    
+    
     def __init__(self):
-        
-        self.graph = ['A','B','C','D','E','F']
-        
-        self.edges = {['A','B'],11,
-                      ['A','C'],16,
-                      ['B','C'],14,
-                      ['B','D'],18,
-                      ['C','D'],10,
-                      ['C','E'],17,
-                      ['D','E'],15,
-                      ['D','F'],12,
-                      ['E','F'],16}
-        
+        self.graph = adjacency_matrix.graph
         self.unvisited_nodes = []
+        self.destination = str()
+        self.location = str('A')
+        self.nodes = adjacency_matrix.vertices
+        
+        self.selectDestination()
+        
+        
+    def markNodesUnvisited(self):
+        for node in self.nodes:
+            if node == self.location:
+                pass
+            else:
+                self.unvisited_nodes.append(node)
+                
+    
+    def tentativeDistance(self, node):
+        if node == self.location:
+            return int(0)
+        else:
+            return float(inf)
+        
+        
+    def selectDestination(self):
+        
+        destination_found = False
+        
+        print('Graph representation:')
+        self.printGraphRepresentation()
+        print()
+        
+        while destination_found == False:
+            
+            self.destination = str(input(f'\nYour current location is {self.location}. Enter destination: ').upper())
+
+            if self.destination == self.location:
+                print('\nDestination cannot be your current location. Pick again.')
+                
+            else:
+                destination_found = True
+        
+        print(f'\nYou are travelling from {self.location} to {self.destination}.')
+        self.markNodesUnvisited()
+    
+    
+    def printGraphRepresentation(self):
+        adjacency_matrix.print_graph()
+        for i in self.graph:
+            print(i)
+        
+Djikstra()
