9053a2e210dbd44f47bc1abbc9634af2c1aab714
James McCann <58793330+pauchxk@users.noreply.github.com>
2023-10-20 20:26:14 +0100

    Update adjacency_matrix.py

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -3,34 +3,31 @@
 #I have just cleaned up + added data, in order to understand graph
 #traversal within Python.
 
-#need to change terminology of variables so that the two programs match.
-#vertices = nodes
-#vertex = node
 #need to change syntax of functions vs variables:
 #variables = this_is_var
 #functions = thisIsFunc
 
-# Add a vertex to the set of vertices and the graph
+# Add a vertex to the set of nodes and the graph
 def add_vertex(v):
     
   global graph
-  global vertices_no
-  global vertices
+  global nodes_no
+  global nodes
   
-  if v in vertices:
-    print("Vertex ", v, " already exists")
+  if v in nodes:
+    print("Node ", v, " already exists")
     
   else:
-    vertices_no = vertices_no + 1
-    vertices.append(v)
+    nodes_no = nodes_no + 1
+    nodes.append(v)
     
-    if vertices_no > 1:
+    if nodes_no > 1:
         for vertex in graph:
             vertex.append(0)
             
     temp = []
     
-    for i in range(vertices_no):
+    for i in range(nodes_no):
         temp.append(0)
         
     graph.append(temp)
@@ -39,45 +36,45 @@ def add_vertex(v):
 def add_edge(v1, v2, e):
     
     global graph
-    global vertices_no
-    global vertices
+    global nodes_no
+    global nodes
     
     # Check if vertex v1 is a valid vertex
-    if v1 not in vertices:
-        print("Vertex ", v1, " does not exist.")
+    if v1 not in nodes:
+        print("Node ", v1, " does not exist.")
         
     # Check if vertex v1 is a valid vertex
-    elif v2 not in vertices:
-        print("Vertex ", v2, " does not exist.")
+    elif v2 not in nodes:
+        print("Node ", v2, " does not exist.")
         
     # Since this code is not restricted to a directed or 
     # an undirected graph, an edge between v1 v2 does not
     # imply that an edge exists between v2 and v1
     else:
-        index1 = vertices.index(v1)
-        index2 = vertices.index(v2)
+        index1 = nodes.index(v1)
+        index2 = nodes.index(v2)
         graph[index1][index2] = e
 
 # Print the graph
 def print_graph():
     
   global graph
-  global vertices_no
+  global nodes_no
   
-  for i in range(vertices_no):
-    for j in range(vertices_no):
+  for i in range(nodes_no):
+    for j in range(nodes_no):
       if graph[i][j] != 0:
-        print(vertices[i], " -> ", vertices[j], \
+        print(nodes[i], " -> ", nodes[j], \
         " Edge weight: ", graph[i][j])
      
-# stores the vertices in the graph
-vertices = []
+# stores the nodes in the graph
+nodes = []
 
-# stores the number of vertices in the graph
-vertices_no = 0
+# stores the number of nodes in the graph
+nodes_no = 0
 graph = []
 
-# Add vertices to the graph
+# Add nodes to the graph
 add_vertex('A')
 add_vertex('B')
 add_vertex('C')
@@ -85,7 +82,7 @@ add_vertex('D')
 add_vertex('E')
 add_vertex('F')
 
-# Add the edges between the vertices by specifying
+# Add the edges between the nodes by specifying
 # the from and to vertex along with the edge weights.
 add_edge('A', 'B', 11)
 add_edge('A', 'C', 16)
